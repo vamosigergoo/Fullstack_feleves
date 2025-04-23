1bff0b6bbf507bb6a6989d882111cfaf27f4d13d
Gergő Vámosi <vamosi.gergo04@gmail.com>
2025-04-23 14:46:04 +0200

    program_cs

--- jateklepes_backend/Program.cs
+++ jateklepes_backend/Program.cs
@@ -1,6 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://127.0.0.1:5500") // Frontend alapértelmezett URL-je
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseStaticFiles();
+
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
