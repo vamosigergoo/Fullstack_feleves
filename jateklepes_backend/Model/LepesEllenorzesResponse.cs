a59cbb1b1d5ee81c0ddb0775da615e8251dc4c15
Gergő Vámosi <vamosi.gergo04@gmail.com>
2025-04-23 13:56:57 +0200

    response

--- jateklepes_backend/Model/LepesEllenorzesResponse.cs
+++ jateklepes_backend/Model/LepesEllenorzesResponse.cs
@@ -2,5 +2,9 @@
 {
     public class LepesEllenorzesResponse
     {
+        public char[][] UjJatekTer { get; set; }
+        public int FelvettCoin { get; set; }
+        public string Hiba { get; set; }
+        public string Uzenet { get; set; }
     }
 }
